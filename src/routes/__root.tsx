fd651f9a64173e9061b0b477f068436cf0e56f09
darwishathibi <danish.darwis@live.iium.edu.my>
2024-12-16 02:27:24 +0800

    create button components

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,76 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "/about",
+    icon: Inbox,
+  },
+  {
+    title: "Test",
+    url: "/test",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<div className="p-2 flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Home
-				</Link>{" "}
-				<Link to="/about" className="[&.active]:font-bold">
-					About
-				</Link>
-			</div>
-			<hr />
-			<Outlet />
-		</>
-	),
+  component: () => (
+    <SidebarProvider>
+      <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>
+              <p className="text-lg ">Mengah Holding</p>
+            </SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url} className="">
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+      <div className="mt-3 ml-2">
+        <SidebarTrigger />
+      </div>
+      <hr />
+      <Outlet />
+    </SidebarProvider>
+  ),
 });
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,25 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { ComponentChart } from "@/components/part/componentChart";
+import AppTable from "@/components/part/app-table";
+import AppCards from "@/components/part/app-cards";
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
 function Index() {
   return (
-    <main className="border border-gray-200 rounded-lg p-4 h-full w-full">
+    <main className="h-full w-full mt-4 p-4 space-y-4">
+      <div>
+        <h1 className="text-2xl font-semibold">Dashboard</h1>
+        <p className="text-sm text-gray-500">
+          Welcome back, Darwis! Here's what's happening with your projects today.
+        </p>
+      </div>
+      <AppCards />
       <ComponentChart />
+      <AppTable />
     </main>
   );
 }
+
+
