83be560277a627d6b80e4a064b28e94ae9d364d4
darwishathibi <danish.darwis@live.iium.edu.my>
2024-12-17 12:17:45 +0800

    major modify

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,76 +1,9 @@
-import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
 
-const items = [
-  {
-    title: "Home",
-    url: "/",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "/about",
-    icon: Inbox,
-  },
-  {
-    title: "Test",
-    url: "/test",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
 export const Route = createRootRoute({
   component: () => (
-    <SidebarProvider>
-      <Sidebar>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupLabel>
-              <p className="text-lg ">Mengah Holding</p>
-            </SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <Link href={item.url} className="">
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
-      <div className="mt-3 ml-2">
-        <SidebarTrigger />
-      </div>
-      <hr />
+    <>
       <Outlet />
-    </SidebarProvider>
+    </>
   ),
 });
